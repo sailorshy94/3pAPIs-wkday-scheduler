ff86945070abbfbcdfd3a3132ab5e8509c12516b
sailorshy94 <sbutle94@gmail.com>
2023-11-20 15:15:34 -0500

    created variables for input element (textarea) and user input value (text entered into textarea and then saved with button), tested with console logs

--- script.js
+++ script.js
@@ -1,37 +1,33 @@
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
+//  How might the id be
   // useful when saving the description in local storage?
-
   var timeBlockContainer = $(".container-lg");
-  // var buttonEl = $(".saveBtn");
-  // can do $(".saveBtn") inside beside "click"
 
   timeBlockContainer.on("click", ".saveBtn", function (event) {
     // there should be a way to do this to only grab textareas with text inside???
     event.preventDefault(); //event default - any click then stops 
 
     var clickTarget = $(this);
-    // console.log(clickTarget);
-    var timeBlock = clickTarget.parent();
-    
-   console.log(timeBlock);
-    // $(".description").each(function () {
-    //   console.log($(this).val());
-    // need to be able to set the key as hour #, how to do that? Use the id but how to reference it here?
-    //   localStorage.setItem("hour-", JSON.stringify(userInput));
-    // })
-    // var hourBlock = $("#hour-");
-    // console.log(hourBlock.children());
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
+
+    var timeBlockEl = clickTarget.parent();
+
+    var inputEl = clickTarget.prev();
+
+    var userInput = inputEl.val();
+
+    console.log(userInput);
   })
+
+  // TODO: Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. HINTS: How can the id
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
+
+// ????
+  // .addClass("past");
+  // .addClass("present");
+  // .addClass("future"); 
 
   var currentTime = dayjs("hh:mm:ss");
   // var times = [
