2a6f9280837852a9473ca393b026c6355d03b951
sailorshy94 <sbutle94@gmail.com>
2023-11-20 21:50:53 -0500

    instructor helped refactor for loop for timeblock classes dependent on time, fixed id selector

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $(function () {
+
   var timeBlockContainer = $(".container-lg");
 
   timeBlockContainer.on("click", ".saveBtn", function (event) {
@@ -10,32 +11,23 @@ $(function () {
 
     var timeId = clickTarget.parent().attr("id");
 
-    // console.log(timeId); //returns the string hour-#, how to use for local storage?
-
     var inputEl = clickTarget.prev();
 
     var userInput = inputEl.val();
 
-    // console.log(userInput);
-
-    // for local storage, id name (hour-#) is the key, userinput = value for (key, val) pair
     localStorage.setItem(timeId, userInput);
   })
 
   var currentTime = dayjs().hour();
 
   for (i = 9; i <= 17; i++) {
-    var timeBlockEl = $("#hour" + i);
-    if (i > currentTime) {
+    var timeBlockEl = $("#hour-" + i);
+    if (i < currentTime) {
       timeBlockEl.addClass("past");
+    } else if (i > currentTime) {
+      timeBlockEl.addClass("future");
     } else {
-      if (i < currentTime) {
-        timeBlockEl.addClass("future");
-      } else {
-        if (i === currentTime) {
-          timeBlockEl.addClass("present");
-        }
-      }
+      timeBlockEl.addClass("present");
     }
   }
 
@@ -45,5 +37,4 @@ $(function () {
   //
   var today = dayjs();
   $("#currentDay").text(today.format("dddd, MMMM D, YYYY"));
-
 });
