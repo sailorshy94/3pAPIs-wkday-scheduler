47bbeeb613a64e8a24e72fc0a3742c930978039f
sailorshy94 <sbutle94@gmail.com>
2023-11-21 01:25:19 -0500

    attempted to create functions to enable data from local storage to display to webpage and persist using input from LA, however unable to apply successfully, commented out

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 $(function () {
 
   var timeBlockContainer = $(".container-lg");
- 
+
   timeBlockContainer.on("click", ".saveBtn", function (event) {
     event.preventDefault(); //event default - any click then stops 
 
     var clickTarget = $(this);
     var timeId = clickTarget.parent().attr("id");
     var userInput = clickTarget.siblings(".description").val();
-    console.log(userInput);
-
-    // localStorage.setItem(timeId, userInput);
-  })
+    // console.log(userInput);
+    localStorage.setItem(timeId, userInput);
+  });
 
   var currentTime = dayjs().hour();
 
@@ -26,12 +25,27 @@ $(function () {
     }
   }
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+  // function loadData() {
+  //   for (var i = 9; i <= 17; i++) {
+  //     var key = $("#hour-") + i;
+  //     var data = localStorage.getItem("key");
 
+  //     if (data !== null) {
+  //       updateEl(key, data);
+  //     }
+  //   }
+  // }
+
+  // function updateEl(key, data) {
+  //   if (key === $("#hour-" + i)) {
+  //     $(".description").innerHTML(data);
+  //   }
+  // }
 
   var today = dayjs();
   $("#currentDay").text(today.format("dddd, MMMM D, YYYY"));
+  
+  // $(document).ready(function () {
+  //   loadData();
+  // })
 });
